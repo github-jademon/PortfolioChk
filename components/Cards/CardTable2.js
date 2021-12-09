1b1f6b7ee6a9113555a7c6c82be249f5e1b7953f
jademon <j2python@gmail.com>
2021-12-10 00:05:58 +0900

    add modal

--- components/Cards/CardTable2.js
+++ components/Cards/CardTable2.js
@@ -7,6 +7,7 @@ import { createPopper } from "@popperjs/core";
 
 // import TableDropdown from "components/_Dropdowns/TableDropdown.js";
 // import { data } from "autoprefixer";
+import Modal from "../modal/Modal.js";
 
 export default function CardTable2({ color }) {
   const userid = "1113";
@@ -30,6 +31,9 @@ export default function CardTable2({ color }) {
     setDropdownPopoverShow(false);
   };
   const [chgbtn, setchgbtn] = React.useState(0);
+
+  const [showModal, setShowModal] = React.useState(false);
+
   return (
     <>
       <div
@@ -52,6 +56,48 @@ export default function CardTable2({ color }) {
             </div>
           </div>
         </div>
+        {showModal ? (
+          <>
+            <div
+              className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+            >
+              <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                {/*content*/}
+                <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                  {/*header*/}
+                  <div className="flex items-start justify-between px-5 py-3 border-b border-solid border-blueGray-200 rounded-t">
+                    <h3 className="text-lg font-semibold mt-1">
+                      파일을 선택해주세요
+                    </h3>
+                    <button
+                      className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                      onClick={() => setShowModal(false)}
+                    >
+                      <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                        ×
+                      </span>
+                    </button>
+                  </div>
+                  {/*body*/}
+                  <div className="relative p-6 flex-auto">
+                    <input type="file" id="file" />
+                  </div>
+                  {/*footer*/}
+                  <div className="flex items-center justify-end px-6 py-1 border-t border-solid border-blueGray-200 rounded-b">
+                    <button
+                      className="text-indigo-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                      type="button"
+                      onClick={() => setShowModal(false)}
+                    >
+                      확인
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+          </>
+        ) : null}
         <div className="block w-full overflow-x-auto">
           {/* Projects table */}
           <table className="items-center w-full bg-transparent border-collapse">
@@ -105,7 +151,7 @@ export default function CardTable2({ color }) {
                   <a href={ userurl } className={"w-full p-2 " + (chgbtn === 1 ? "hidden" : "")} target="_blank">
                     { userurl }
                   </a>
-                  <input className={"text-left w-full focus:outline-none focus:border-indigo-500 border-b border-gray-400 h-full p-2 " + (chgbtn === 0 ? "hidden" : "")} defaultValue={ userurl } spellCheck={false} placeholder="포트폴리오 주소를 입력해주세요" />
+                  <input className={"text-left w-full focus:outline-none focus:border-indigo-500 border-b border-gray-400 h-full p-2 " + (chgbtn === 0 ? "hidden" : "")} defaultValue={ userurl } spellCheck={false} placeholder="주소를 입력해주세요" />
                 
                 </td>
                 <td className={"border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right "}>
@@ -132,7 +178,7 @@ export default function CardTable2({ color }) {
                       className={
                         (dropdownPopoverShow ? "block " : "hidden ") +
                         "bg-white text-base z-50 float-left list-none text-left rounded shadow-lg min-w-42 " 
-                        + (chgbtn == 1 ? "hidden" : "")
+                        + (chgbtn != 0 ? "hidden" : "")
                       }
                     >
                       <a
@@ -149,10 +195,11 @@ export default function CardTable2({ color }) {
                         className={
                           "text-sm py-2 px-6 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700 border-t"
                         }
-                        onClick={(e) => {e.preventDefault(); console.log(chgbtn); setchgbtn(chgbtn?0:1)}}
+                        onClick={(e) => {e.preventDefault(); console.log(chgbtn); setShowModal(true)}}
                       >
                         file
                       </a>
+                      
                     </div>
                   </div>
                 </td>
