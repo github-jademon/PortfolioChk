e1fafbad098ecb73bd4af34cadcd53dc71314246
jademon <j2python@gmail.com>
2021-12-10 21:07:24 +0900

    comb admin, index

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,38 @@
 import React from "react";
+import { useRouter } from 'next/router'
 
-import Table from "components/Table/Table.js";
+import AdminNavbar from "components/Navbars/Navbar.js";
+import Sidebar from "components/Sidebar/Sidebar.js";
+import HeaderStats from "components/Headers/HeaderStats.js";
+import Table from "pages/[gradeid]/[classid].js";
 import Table2 from "components/Table/Table2.js";
 
-import Admin from "layouts/Admin.js";
-
-export default function Tables({ userid, classname }) {
+const Index = () => {
+// export default function Index() {
+  const router = useRouter();
+  const { classid } = router.query;
+  let userid = "1113";
+  let username = "윤서준";
   return (
     <>
-      <div className="flex flex-wrap mt-7">
-        <div className="w-full mb-12 px-4">
-          <Table2 userid={userid} />
+      <Sidebar />
+        <div className="relative md:ml-64 bg-blueGray-100">
+          <AdminNavbar userid={userid} username={username} />
+          {/* Header */}
+          <HeaderStats />
+          <div className="px-4 md:px-10 mx-auto w-full -m-24">
+            <div className="flex flex-wrap mt-7">
+              <div className="w-full mb-12 px-4">
+                <Table2 userid={userid} />
+              </div>
+              <div className="w-full mb-12 px-4">
+                <Table userid={userid} classid={classid} />
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="w-full mb-12 px-4">
-          <Table userid={userid} classname={classname} />
-        </div>
-      </div>
     </>
   );
 }
 
-Tables.layout = Admin;
+export default Index;
