dbf8c4f0ae97ae0f2d355578fbad4241172664dc
jademon <j2python@gmail.com>
2021-12-10 21:10:45 +0900

    modify navbar name

--- components/Navbars/Navbar.js
+++ components/Navbars/Navbar.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-// import UserDropdown from "components/Dropdowns/UserDropdown.js";
-
 export default function Navbar({ username }) {
   const login = () => {
     fetch("https://portfolio.gbsw.hs.kr:25565/login").then((response) =>
@@ -16,10 +14,8 @@ export default function Navbar({ username }) {
   const userid="";
   return (
     <>
-      {/* Navbar */}
       <nav className="absolute top-0 left-0 w-full z-10 bg-transparent md:flex-row md:flex-nowrap md:justify-start flex items-center p-4">
         <div className="w-full mx-autp items-center flex justify-between md:flex-nowrap flex-wrap md:px-10 px-4">
-          {/* Brand */}
           <a
             className="text-white text-sm uppercase hidden lg:inline-block font-semibold"
             href="#pablo"
@@ -27,22 +23,7 @@ export default function Navbar({ username }) {
           >
             포트폴리오
           </a>
-          {/* Form */}
-          {/* <form className="md:flex hidden flex-row flex-wrap items-center lg:ml-auto mr-3">
-            <div className="relative flex w-full flex-wrap items-stretch">
-              <span className="z-10 h-full leading-snug font-normal absolute text-center text-blueGray-300 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3">
-                <i className="fas fa-search"></i>
-              </span>
-              <input
-                type="text"
-                placeholder="Search here..."
-                className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:ring w-full pl-10"
-              />
-            </div>
-          </form> */}
-          {/* User */}
           <ul className="flex-col md:flex-row list-none items-center hidden md:flex text-white text-sm">
-            {/* <UserDropdown /> */}
             <div onClick={(e) => {userid ? logout() : login();}}>
               { userid ? userid + " " + username : "로그인"}
             </div>
@@ -50,7 +31,6 @@ export default function Navbar({ username }) {
           </ul>
         </div>
       </nav>
-      {/* End Navbar */}
     </>
   );
 }
